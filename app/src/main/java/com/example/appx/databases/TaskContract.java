988adaacf10806b356a71c7998335a054d863417
Glebasta33 <gleb.trusow@yandex.ru>
2021-06-01 08:03:23 +0300

    tasks.db

---
+++ app/src/main/java/com/example/appx/databases/TaskContract.java
@@ -0,0 +1,25 @@
+package com.example.appx;
+
+import android.provider.BaseColumns;
+
+
+
+public class TaskContract {
+    public static final class TasksEntry implements BaseColumns {
+        public static String TABLE_NAME = "tasks";
+        public static final String ITEM_COLUMN_CONTENT = "content";
+        public static final String ITEM_COLUMN_PERSON = "person";
+        public static final String ITEM_COLUMN_STATUS = "status";
+
+        public static final String TYPE_TEXT = "TEXT";
+        public static final String TYPE_INTEGER = "INTEGER";
+
+        public static final String CREATE_TASKS_TABLE_COMMAND = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME +
+                "(" + _ID + " " + TYPE_INTEGER + " PRIMARY KEY AUTOINCREMENT, " +
+                ITEM_COLUMN_CONTENT + " " + TYPE_TEXT + ", " +
+                ITEM_COLUMN_PERSON + " " + TYPE_TEXT + ", " +
+                ITEM_COLUMN_STATUS + " " + TYPE_TEXT + " )";
+
+        public static final String DROP_COMMAND = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+}
---
+++ app/src/main/java/com/example/appx/databases/TasksDBHelper.java
@@ -0,0 +1,29 @@
+package com.example.appx;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class TasksDBHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "tasks.db";
+    public static final int DB_VERSION = 7;
+
+    public TasksDBHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TaskContract.TasksEntry.CREATE_TASKS_TABLE_COMMAND);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(TaskContract.TasksEntry.DROP_COMMAND);
+        onCreate(db);
+    }
+}
